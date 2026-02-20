cb7b28edf87b9e1f365ed69772890ef81b4eb991
rehan-19 <rehannshaikh77@gmail.com>
2026-02-21 02:00:46 +0530

    UC3: Refactored banner using String.join() for memory efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,40 @@
-/**
- * OOPSBannerApp
- *
- * Prints OOPS in Banner format.
- *
- * @author Rehan
- * @version 2.0
- */
-
 public class OOPSBannerApp {
 
- 
     public static void main(String[] args) {
-        // Each letter (O, O, P, S) is 9 characters wide, aligned properly
-        System.out.println("  *****   " + "  *****   " + " ******** " + " ******** ");
-        System.out.println(" *     *  " + " *     *  " + " *       *" + " *        ");
-        System.out.println(" *     *  " + " *     *  " + " *       *" + " *        ");
-        System.out.println(" *     *  " + " *     *  " + " ******** " + " ******** ");
-        System.out.println(" *     *  " + " *     *  " + " *         " + "       *  ");
-        System.out.println(" *     *  " + " *     *  " + " *         " + "       *  ");
-        System.out.println("  *****   " + "  *****   " + " *         " + " ******** ");
+
+        String[] O = {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+
+        String[] P = {
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+
+        String[] S = {
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+
+        for (int i = 0; i < 7; i++) {
+            String line = String.join("   ", O[i], O[i], P[i], S[i]);
+            System.out.println(line);
+        }
     }
-} 
+}
