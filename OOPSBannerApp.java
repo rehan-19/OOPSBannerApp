4c744c5a3219aa94ceafa8bea3b674d256689a7d
rehan-19 <rehannshaikh77@gmail.com>
2026-02-20 23:36:50 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerApp
+ * 
+ * This program prints "OOPS" to the console.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
