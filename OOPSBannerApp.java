a8b56f6f0af5902a4f7aa12f041ea5d56641c4a4
rehan-19 <rehannshaikh77@gmail.com>
2026-03-11 19:58:14 +0530

    UC5: Combined array declaration and initialization using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,44 +2,50 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] O = {
-                " ***** ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                " ***** "
+        String[] banner = {
+            String.join("   ", 
+                " ***** ", 
+                " ***** ", 
+                " ******", 
+                " ******"),
+
+            String.join("   ", 
+                "*     *", 
+                "*     *", 
+                "*     *", 
+                "*     "),
+
+            String.join("   ", 
+                "*     *", 
+                "*     *", 
+                "*     *", 
+                "*     "),
+
+            String.join("   ", 
+                "*     *", 
+                "*     *", 
+                "****** ", 
+                " ***** "),
+
+            String.join("   ", 
+                "*     *", 
+                "*     *", 
+                "*      ", 
+                "      *"),
+
+            String.join("   ", 
+                "*     *", 
+                "*     *", 
+                "*      ", 
+                "      *"),
+
+            String.join("   ", 
+                " ***** ", 
+                " ***** ", 
+                "*      ", 
+                " ***** ")
         };
 
-        String[] P = {
-                "****** ",
-                "*     *",
-                "*     *",
-                "****** ",
-                "*      ",
-                "*      ",
-                "*      "
-        };
-
-        String[] S = {
-                " ***** ",
-                "*      ",
-                "*      ",
-                " ***** ",
-                "      *",
-                "      *",
-                " ***** "
-        };
-
-        // UC4: Store full banner lines in array
-        String[] banner = new String[7];
-
-        for (int i = 0; i < 7; i++) {
-            banner[i] = String.join("   ", O[i], O[i], P[i], S[i]);
-        }
-
-        // Print using enhanced for-loop
         for (String line : banner) {
             System.out.println(line);
         }
