a431bebc011fc8ad255fd0f6ffa5c82b5c44ec0d
rehan-19 <rehannshaikh77@gmail.com>
2026-03-11 21:04:35 +0530

    UC6: Implemented static helper methods for O, P, S

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,46 +2,53 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] O = {
-                " ***** ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                " ***** "
-        };
-
-        String[] P = {
-                "****** ",
-                "*     *",
-                "*     *",
-                "****** ",
-                "*      ",
-                "*      ",
-                "*      "
-        };
-
-        String[] S = {
-                " ***** ",
-                "*      ",
-                "*      ",
-                " ***** ",
-                "      *",
-                "      *",
-                " ***** "
-        };
-
-        // UC4: Store full banner lines in array
-        String[] banner = new String[7];
+        String[] o = getO();
+        String[] p = getP();
+        String[] s = getS();
 
         for (int i = 0; i < 7; i++) {
-            banner[i] = String.join("   ", O[i], O[i], P[i], S[i]);
+            System.out.println(
+                String.join("   ", o[i], o[i], p[i], s[i])
+            );
         }
+    }
 
-        // Print using enhanced for-loop
-        for (String line : banner) {
-            System.out.println(line);
-        }
+    // Method for letter O
+    public static String[] getO() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Method for letter P
+    public static String[] getP() {
+        return new String[]{
+            "****** ",
+            "*     *",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Method for letter S
+    public static String[] getS() {
+        return new String[]{
+            " ***** ",
+            "*     ",
+            "*     ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
     }
 }
